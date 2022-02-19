8ddf426d7eb36852017b7aac80fc33dca747295a
fomula91 <fomula91@gmail.com>
2022-02-19 21:25:08 +0900

    add step2 ploblem

---
+++ 2_step/14681.js
@@ -0,0 +1,12 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+let input = [];
+rl.on("line", (line) => {
+    input.push(line)
+})
+rl.on("close", () =>{
+    process.exit();
+})
