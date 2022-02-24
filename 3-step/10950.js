cf2cb3ff413465ff0ebaa3df76a7bf8038510170
fomula91 <fomula91@gmail.com>
2022-02-24 21:37:39 +0900

    add ploblem 3-step

---
+++ 3-step/10950.js
@@ -0,0 +1,15 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+    let arr = [];
+rl.on("line", (line) => {
+    arr.push(line.split(' '));
+}).on("close", () =>{
+    for(let i = 1; i <= parseInt(arr[0]); i++){
+        console.log(parseInt(arr[i][0])+parseInt(arr[i][1]))
+    }
+    rl.close();
+    process.exit();
+})
