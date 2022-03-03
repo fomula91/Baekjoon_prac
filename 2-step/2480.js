1fab82d1e489e74f7ec086d764a99ad2bb6c22c5
fomula91 <fomula91@gmail.com>
2022-03-04 00:42:55 +0900

    add 2480.js

--- 2-step/2480.js
+++ 2-step/2480.js
@@ -6,16 +6,51 @@ const rl = readline.createInterface({
 rl.on("line", (line) => {
     let input = [];
     input = line.split(' ').map(el => parseInt(el))
-    if(input[0] === input[1] === input[2]){
-        let result = (10000 + input[0]) * 1000
-        console.log(result)
-    }else if(input[0] === input[1] || input[0] === input[2] || input[1]===input[2] || input[0] === input[2]){
-        console.log("2수 같음 ")
-    }else if(input[0] !== input[1] !== input[2]){
-        console.log("모두 다름")
-    }else{
-        console.log("몰?루")
+    const a = input[0];
+    const b = input[1];
+    const c = input[2];
+    let result;
+    
+    if(a == b && a != c){
+        result = 1000 + (a * 100);
+        console.log(result);
     }
+    else if(a == c && b != c){
+        result = 1000 + ( a * 100);
+        console.log(result);
+    }
+    else if(b == c && a != c){
+        result = 1000 + (b * 100);
+        console.log(result);
+    }
+    else if(a != b && a != c && b != c){
+        if(a > b){
+            if(a > c){
+                result = a * 100;
+                console.log(result);
+            }
+            else{
+                result = c * 100;
+                console.log(result);
+            }
+        }
+        else if(a < b){
+            if(b > c){
+                result = b * 100;
+                console.log(result);
+            }
+            else{
+                result = c * 100;
+                console.log(result);
+            }
+        }
+        
+    }
+    else{
+        result = 10000 + (a * 1000);
+        console.log(result);
+    }
+    
     rl.close()
 }).on("close", () => {
     process.exit()
