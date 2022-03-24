06c888749d98b58ab32cb33a40c6dbc200923b34
fomula91 <fomula91@gmail.com>
2022-03-24 23:49:03 +0900

    add target number ploblem

---
+++ programers-python/target-number.py
@@ -0,0 +1,21 @@
+from collections import deque
+import copy
+num = [1, 1, 1, 1, 1]
+target = 3
+
+def solution(numbers, target):
+    answer = 0
+    numberarr = deque(numbers)
+    def dfs(arr, start, index):
+        if len(arr) == index:
+            if start == target:
+               nonlocal answer
+               answer += 1
+            return
+        dfs(arr, start + arr[index], index + 1)
+        dfs(arr, start - arr[index], index + 1)
+        return
+    dfs(numberarr, 0, 0)
+    return print(answer)
+
+solution(num,target)
